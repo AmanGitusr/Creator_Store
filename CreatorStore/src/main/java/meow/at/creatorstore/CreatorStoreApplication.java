f0cebe8a050215d73568fad12914f07a01264014
aman_tiwari <amanvds789@gmail.com>
2026-06-07 15:49:01 +0530

    Chore: Entity and Environment variable setup.

--- CreatorStore/src/main/java/meow/at/creatorstore/CreatorStoreApplication.java
+++ CreatorStore/src/main/java/meow/at/creatorstore/CreatorStoreApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CreatorStoreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CreatorStoreApplication.class, args);
 	}
